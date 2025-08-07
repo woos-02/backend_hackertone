de82d810f821c25ebe84a3ee9875c67c89acbf09
woos-02 <wooseong4954@gmail.com>
2025-08-07 19:17:28 +0900

    chore: settings.py + local_settings.py + .env 파일 수정

--- modelproject/modelproject/settings.py
+++ modelproject/modelproject/settings.py
@@ -101,10 +101,17 @@ SPECTACULAR_SETTINGS = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import pymysql
+
+pymysql.install_as_MySQLdb()
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'), # DB(스키마) 이름
+        'USER': config('DB_USER'), # 유저 이름 (root)
+        'PASSWORD': config('DB_PASSWORD'), # DB 비밀번호
+        'HOST': config('DB_HOST'), # DB 엔드포인트
+        'PORT': 3306,
     }
 }
 
