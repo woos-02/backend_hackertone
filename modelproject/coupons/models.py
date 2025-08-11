a236c6e51f82dc51e06b7f1e6e2143ff7d6868be
jiyonggg <kimvergognoso21@hufs.ac.kr>
2025-08-12 04:51:24 +0900

    feat. 쿠폰 관련 모델 1차 구현 (아직 작동 여부 테스트 안함)

--- modelproject/coupons/models.py
+++ modelproject/coupons/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+# 쿠폰북
+
+class Coupon(models.Model):
+    """
+    실제 사용되는 쿠폰입니다.
+    """
+    couponbook = models.ForeignKey("couponbook.models.CouponBook",
+                                   on_delete=models.CASCADE,
+                                   help_text="해당 쿠폰이 등록되어 있는 쿠폰북입니다.")
+    original_template = models.ForeignKey("CouponTemplate",
+                                          on_delete=models.CASCADE,
+                                          help_text="쿠폰 발행에 사용된 쿠폰 템플릿입니다. 유효성 검증에 사용합니다.")
+    image_url = models.URLField(help_text="쿠폰의 이미지가 담겨 있는 URL입니다.")
+    max_stamps = models.PositiveIntegerField(help_text="쿠폰 완성까지 필요한 총 스탬프의 개수입니다.")
+    is_completed = models.BooleanField(default=False, help_text="쿠폰 완성 여부를 나타냅니다.")
+
+class CouponTemplate(models.Model):
+    """
+    점주가 등록해서 게시중인 쿠폰 템플릿입니다.
+    """
+    valid_until = models.DateTimeField(default=None, help_text="쿠폰의 유효기간입니다.")
+    first_n_persons = models.PositiveIntegerField(default=0, help_text="선착순 몇명까지 쿠폰이 발급한지를 의미합니다.")
+    image_url = models.URLField(help_text="쿠폰의 이미지가 담겨 있는 URL입니다.")
+    max_stamps = models.PositiveIntegerField(help_text="쿠폰 완성까지 필요한 총 스탬프의 개수입니다.")
+    is_on = models.BooleanField(default=True, help_text="게시 중/비공개 여부를 불리언으로 나타냅니다.")
+    views = models.PositiveIntegerField(default=0, help_text="조회수를 의미합니다.")
+    saves = models.PositiveIntegerField(default=0, help_text="저장된 횟수입니다.")
+    uses = models.PositiveIntegerField(default=0, help_text="사용된 횟수입니다.")
+
+class RewardsInfo(models.Model):
+    """
+    한 쿠폰의 리워드 정보를 나타냅니다.
+    """
+    coupon_template = models.ForeignKey(CouponTemplate,
+                                        on_delete=models.CASCADE,
+                                        help_text="어떤 쿠폰 템플릿에 있는 리워드 정보인지를 의미합니다.")
+    amount = models.PositiveIntegerField(help_text="리워드를 지급하는 스탬프 횟수입니다.")
+    reward = models.CharField(help_text="어떤 혜택이 있는지를 의미합니다.")
+
+class Stamp(models.Model):
+    """
+    스탬프입니다.
+    """
+    coupon = models.ForeignKey(Coupon,
+                               on_delete=models.CASCADE,
+                               help_text="어떤 쿠폰에 적립된 스탬프인지를 의미합니다.")
+    related_payment = models.ForeignKey("payment.models.PaymentHistory",
+                                        on_delete=models.CASCADE,
+                                        help_text="해당 스탬프의 적립 근거가 되는 결제 내역입니다.")
+    customer = models.ForeignKey("accounts.models.User", on_delete=models.CASCADE, help_text="스탬프를 적립받은 고객입니다.")
