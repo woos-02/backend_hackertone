a71b8893b07690371a0ee25d114e70c75029b888
woos-02 <wooseong4954@gmail.com>
2025-08-15 14:24:43 +0900

    docs: 도커 파일 코드 정리

--- modelproject/modelproject/settings.py
+++ modelproject/modelproject/settings.py
@@ -142,7 +142,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
+AUTH_PASSWORD_VALIDATORS: list[dict[str, str]] = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
