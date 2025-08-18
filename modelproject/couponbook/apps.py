239db6b09af6c81351b1fadcfe5883ecbed7057b
jiyonggg <kimvergognoso21@hufs.ac.kr>
2025-08-18 22:42:43 +0900

    chore. signal import하는 코드 추가

--- modelproject/couponbook/apps.py
+++ modelproject/couponbook/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CouponbookConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'couponbook'
+
+    def ready(self):
+        from . import signals
