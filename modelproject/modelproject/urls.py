8bd2789dec0642ee5996632aa7f7bd97b96537fa
woos-02 <wooseong4954@gmail.com>
2025-08-19 00:30:39 +0900

    큐레이션(pr)까지의 dev 최신화 폴더 woos 브랜치로 동기화

--- modelproject/modelproject/urls.py
+++ modelproject/modelproject/urls.py
@@ -23,7 +23,6 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/accounts/", include("accounts.urls")),
     # OpenAPI 스키마 & UI
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="docs"),
