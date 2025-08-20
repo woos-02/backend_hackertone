1ea8371b905d150b29e7373ca7450f75b59cefb3
jiyonggg <kimvergognoso21@hufs.ac.kr>
2025-08-20 17:31:08 +0900

    fix. 단일 쿠폰 조회 시에 개별 스탬프 url reverse 실패하는 문제 수정

--- modelproject/couponbook/urls.py
+++ modelproject/couponbook/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('couponbooks/<int:couponbook_id>/favorites/', FavoriteCouponListView.as_view(), name='favorite-coupon-list'),
     path('own-couponbook/favorites/<int:favorite_id>/', FavoriteCouponDetailView.as_view(), name='favorite-coupon-detail'),
     path('coupons/<int:coupon_id>/stamps/', StampListView.as_view(), name='stamp-list'),
-    path('stamps/<int:pk>/', StampDetailView.as_view(), name='stamp-detail'),
+    path('stamps/<stamp_id:pk>/', StampDetailView.as_view(), name='stamp-detail'),
 
     # 쿠폰 템플릿 관련 엔드포인트입니다.
     path('coupon-templates/', CouponTemplateListView.as_view(), name='coupon-template-list'),
